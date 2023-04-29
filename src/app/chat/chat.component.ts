f7e2829dc7b443f66753ff03b9b493e71a870dd7
Skovgaard <sk0vgaard@live.dk>
2023-04-29 15:48:03 +0200

    Add better UX with the ChatBot with some css and new structure

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,6 @@
-import {AfterViewChecked, Component, ElementRef, ViewChild} from '@angular/core';
-import { OpenaiService } from "../_services/openai.service";
-import { ChatCompletionRequestMessageRoleEnum } from "openai";
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {OpenaiService} from "../_services/openai.service";
+import {ChatCompletionRequestMessageRoleEnum} from "openai";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
@@ -9,6 +9,7 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatLegacyChipsModule} from "@angular/material/legacy-chips";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {MatIconModule} from "@angular/material/icon";
+import {MatTooltip, MatTooltipModule} from "@angular/material/tooltip";
 
 export class MessageContent {
   type: 'text' | 'code';
@@ -35,20 +36,19 @@ export class Message {
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatInputModule, MatCardModule, MatButtonModule, MatLegacyChipsModule, MatProgressSpinnerModule, MatIconModule],
+  imports: [CommonModule, FormsModule, MatInputModule, MatCardModule, MatButtonModule, MatLegacyChipsModule, MatProgressSpinnerModule, MatIconModule, MatTooltipModule],
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements AfterViewChecked {
-  @ViewChild('messagesContainer', { read: ElementRef }) messagesContainer!: ElementRef;
+export class ChatComponent {
+  @ViewChild('messagesContainer', {read: ElementRef}) messagesContainer!: ElementRef;
 
-  messages: Message[] = [];
-  userInput: string = '';
+  public messages: Message[] = [];
+  public userInput: string = '';
+  private knownHeaders = ['html', 'typescript', 'scss', 'css'];
 
-  constructor(private openaiService: OpenaiService) {}
 
-  ngAfterViewChecked(): void {
-    this.scrollToBottom();
+  constructor(private openaiService: OpenaiService) {
   }
 
   scrollToBottom(): void {
@@ -60,6 +60,12 @@ export class ChatComponent implements AfterViewChecked {
   }
 
   copyToClipboard(text: string) {
+    const lines = text.split('\n');
+    if (this.knownHeaders.includes(lines[0].trim())) {
+      lines.shift();
+      text = lines.join('\n');
+    }
+
     const textarea = document.createElement('textarea');
     textarea.value = text;
     document.body.appendChild(textarea);
@@ -74,11 +80,17 @@ export class ChatComponent implements AfterViewChecked {
       this.messages.push(newMessage);
       this.generateText(newMessage);
       this.userInput = '';
+      setTimeout(() => {
+        this.scrollToBottom();
+      }, 100);
     }
   }
 
   generateText(message: Message) {
-    const prompts = this.messages.map((item: Message) => ({ role: ChatCompletionRequestMessageRoleEnum.User, content: item.text }));
+    const prompts = this.messages.map((item: Message) => ({
+      role: ChatCompletionRequestMessageRoleEnum.User,
+      content: item.text
+    }));
 
     this.openaiService.createCompletion(prompts).then((text: string) => {
       const codeBlockRegex = /```([\s\S]*?)```/g;
@@ -102,4 +114,34 @@ export class ChatComponent implements AfterViewChecked {
       message.loading = false;
     });
   }
+
+  getCodeHeader(text: string): string | null {
+    const lines = text.split('\n');
+    const firstLine = lines[0].trim();
+
+    return this.knownHeaders.includes(firstLine) ? firstLine : null;
+  }
+
+  removeCodeHeader(text: string): string {
+    const header = this.getCodeHeader(text);
+    if (header) {
+      const lines = text.split('\n');
+      lines.shift();
+      return lines.join('\n');
+    }
+
+    return text;
+  }
+
+  showCopiedTooltip(tooltip: MatTooltip) {
+    tooltip.disabled = true;
+    tooltip.message = 'Copied';
+    tooltip.show();
+    setTimeout(() => {
+      tooltip.hide();
+      tooltip.message = 'Copy to clipboard';
+      tooltip.disabled = false;
+    }, 4500);
+  }
+
 }
